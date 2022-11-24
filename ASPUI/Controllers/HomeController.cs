1cc0b24a217832cbf2c689e3c73818c279d72e81
berke <berke@DESKTOP-515MSAO>
2022-11-24 18:55:06 +0300

    Add project files.

---
+++ ASPUI/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using ASPUI.Models;
+using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASPUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        ICategoryService categoryService;
+        IProductService productService;
+
+        public HomeController(ICategoryService categoryService, IProductService productService)
+        {
+            this.categoryService = categoryService;
+            this.productService = productService;
+        }
+
+        public IActionResult Index(int id)
+        {
+            HomeViewModel model = new HomeViewModel();
+            if (id == 0)
+            {
+                model.products = productService.GetAll();
+            }
+            else
+            {
+                model.products = productService.GetAll(id);
+            }
+            model.categories = categoryService.GetAll();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string name)
+        {
+            HomeViewModel model = new HomeViewModel();
+            model.products = productService.GetAllByDescription(name);
+            model.categories = categoryService.GetAll();
+            return View(model);
+        }
+        public IActionResult Details(int id)
+        {
+            var product = productService.GetById(id);
+            return View(product);
+        }
+    }
+}
